49468333ab4096e79e6ad02e590da0a8d4ef6730
gabe <gabrielius@upcastsocial.com>
2016-02-26 14:54:52 +0000

    frontend almos done

--- index.js
+++ index.js
@@ -5,28 +5,13 @@ var app = express();
 
 app.engine('html', mustacheExpress());          // register file extension mustache
 app.set('view engine', 'html');                 // register file extension for partials
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/frontend/views');
 //app.use(express.static(__dirname + '/public')); // set static folder
 
 
 app.get('/', function (req, res) {
 	res.render('index', {});
 });
-
-app.get('/getStats', function(req, res) {
-	fs.readFile('./data.json', function(err, data) {
-		if(err) {
-			console.log(err);
-		}
-
-
-		var data = JSON.parse(data.toString());
-		data = data.slice(data.length - 480, data.length - 1); // last 8 hours
-		data = data.map(function(obj) { return {'temperature' : (obj.temperature / 1000).toFixed(2), 'date' : obj.date} });
-		res.send(data);
-	});
-});
-
 
 
 app.listen(3000, function () {
